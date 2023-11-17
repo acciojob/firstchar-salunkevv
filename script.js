1ac802c9736fb3376c44bfb9cc5adb2ee8ade466
salunkevv <86159929+salunkevv@users.noreply.github.com>
2023-11-17 11:46:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function firstChar(text) {
   // your code here
+	text=text.trimStart();
+	return text.charAt(0);
 }
 
 // Do not change the code below
 
-const text = prompt("Enter text:");
-alert(firstChar(text));
+// const text = prompt("Enter text:");
+// alert(firstChar(text));
